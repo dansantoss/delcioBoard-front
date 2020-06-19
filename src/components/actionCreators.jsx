5cae2069695f2e1ddee1851cfe5b03494d68bfee
Daniel Santos <daniel.santos399@etec.sp.gov.br>
2020-06-18 21:32:53 -0300

    Alteração da rota da api

--- src/components/actionCreators.jsx
+++ src/components/actionCreators.jsx
@@ -5,7 +5,7 @@ export const getAllDashAtributtes = () => {
     
     return dispatch => {
         axios
-            .get(`http://localhost:3001/comments`)
+            .get(`http://localhost:3001/v1/api/read`)
             .then(response => {
                 let payload = response.data;
                 dispatch({ type: GETDASH, payload });
